6b184f1a1f1e64c32d4527765053c8c2a6ada584
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2021-05-20 14:31:10 -0400

    Se agrego el servicio del crud

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,28 +1,28 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { CreatePostDto } from './dtos';
 import { EditPostDto } from './dtos/edit-post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
 
+    constructor(private readonly postService: PostService) {}
+
     @Get()
     getMany() {
-        return 'OK';
+        return this.postService.getMany();
     }
 
     @Get(':id')
     getOne(@Param('id', ParseIntPipe) id: number) {
-        console.log(typeof id);
-        return {
-            message: 'getOne'
-        };
+        return this.postService.getOne(id);
     }
 
     @Post()
     createOne(
         @Body() dto: CreatePostDto
     ){
-        return dto;
+        return this.postService.createOne();
     }
 
     @Put(':id')
@@ -30,8 +30,11 @@ export class PostController {
         @Param('id', ParseIntPipe) id: number,
         @Body() dto: EditPostDto
     ){
-        return dto;
+        return this.postService.editOne(id);
     }
 
-    deleteOne(){}
+    @Delete(':id')
+    deleteOne(@Param('id', ParseIntPipe) id: number){
+        return this.postService.deleteOne(id);
+    }
 }
